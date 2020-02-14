fa64757e534fd8230127ae3e50adf43a2eb0c7db
wpy8518 <851830700@qq.com>
2020-02-14 10:53:30 +0800

    init repo

---
+++ src/test/java/com/wpy/mycommunity/mycommunity/MycommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wpy.mycommunity.mycommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
