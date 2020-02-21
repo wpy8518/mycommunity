a3f73b6d0c775b189046eac53ffdf575073b286e
wpy8518 <851830700@qq.com>
2020-02-21 09:28:49 +0800

    add check cookies

--- src/main/java/com/wpy/mycommunity/mapper/UserMapper.java
+++ src/main/java/com/wpy/mycommunity/mapper/UserMapper.java
@@ -4,9 +4,13 @@ package com.wpy.mycommunity.mapper;
 import com.wpy.mycommunity.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
     @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
+
+    @Select("select * from user where token = #{token}")
+    User findByToken(String token);
 }
